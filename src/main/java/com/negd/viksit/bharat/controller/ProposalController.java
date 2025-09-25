e1185e658cfc3b345ff9bfbbd384cc1d96a85fb5
Jeetendra Shrivastava <jeetendra.shrivastava@in.ey.com>
2025-09-26 00:50:36 +0530

    fix

--- src/main/java/com/negd/viksit/bharat/controller/ProposalController.java
+++ src/main/java/com/negd/viksit/bharat/controller/ProposalController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/proposals")
+@RequestMapping("/api/other-proposals")
 public class ProposalController {
 
     private final ProposalService service;
